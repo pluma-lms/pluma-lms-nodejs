fe96ea84d7d23a28fa6d8cd63424b8b1a1f13a76
champfish <champfish@users.noreply.github.com>
2015-12-12 23:18:01 -0600

    Update chat.js

--- chat.js
+++ chat.js
@@ -1,12 +1,16 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
-function main()
-{
-    console.log('init chat');
+var fs = require('fs');
+
+function main (socket) {
+     fs.readFile('./chat.html', 'utf8', function (err, data) {
+                if (err) {
+                    console.log(err);
+                }
+                socket.emit('page', data);
+            });
     var nick = socket.request.connection.remoteAddress;
     var title = 'Chat';
-    io.emit('title', title);
+    socket.emit('title', title);
+    socket.emit('servermsg','Welcome '+nick+'.');
     console.log(nick + ':' + socket.request.connection.remotePort + ' connected.');
     socket.on('chat message', function (msg) {
         var command = msg.substring(0, msg.indexOf(' '));
@@ -14,19 +18,20 @@ function main()
         switch (command) {
             case '/nick':
                 var newNick = params;
-                io.emit('servermsg', nick + ' changed his name to ' + newNick);
+                socket.emit('servermsg', nick + ' changed his name to ' + newNick);
                 nick = newNick;
                 break;
             case '/title':
-                io.emit('title', params);
-                io.emit('servermsg', nick + ' changed the title to ' + params);
+                socket.emit('title', params);
+                socket.emit('servermsg', nick + ' changed the title to ' + params);
                 break;
             default:
                 console.log(nick + ': ' + msg);
-                io.emit('servermsg', nick + ': ' + msg);
+                socket.emit('servermsg', nick + ': ' + msg);
         }
     });
     socket.on('disconnect', function () {
         console.log(nick + ' disconnected.');
     });
 }
+module.exports.main = main;
