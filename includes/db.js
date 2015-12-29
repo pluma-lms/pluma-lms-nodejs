b100dac28d794924638dfacf54da9e49efa3550b
champfish <cj.duffee@gmail.com>
2015-12-28 21:53:55 -0600

    Hopefully didn't permantly destroy everything

--- includes/db.js
+++ includes/db.js
@@ -1,18 +1,19 @@
 var db = null;
-
+var students = null;
 var MongoClient = require('mongodb').MongoClient;
-var url = 'mongodb://localhost:27017/students';
+var url = 'mongodb://localhost:27017/pluma';
 MongoClient.connect(url, function (err, initDb) {
     if (err !== null) {
         console.log(err);
     }
     db = initDb;
+    students = db.collection('students');
     // db.close();                                                                                                                                                                                                                                                                                                                                                                                                                                                           process.exit(1337);
-    //Don't uncomment the above line. Do not uncomment it on pain of death. It may seem like good practice to but if you remove those two slashes, the application will break, the server will catch on fire, and the dead shall rise. You have been warned.
+    //Don't uncomment the above line. Do not uncomment it on pain of death. It may seem like good practice to, but if you remove those two slashes, the application will break, the server will catch on fire, and the dead shall rise. You have been warned.
 });
 
 function findUser(username, callback) {
-    db.collection('students').findOne({"username": username}, function (err, user) {
+    students.findOne({"username": username}, function (err, user) {
         if (err !== null) {
             console.log(err);
         }
@@ -20,4 +21,18 @@ function findUser(username, callback) {
     });
 }
 
+function registerUser(userInfo) {
+            students.insertOne(
+            {"username": userInfo[0], "password": userInfo[1], "name": userInfo[2], "gender": userInfo[3], "dob": userInfo[4], "studentId": userInfo[5]},
+            function (err, save) {
+                if (err !== null) {
+                    console.log(err);
+                } else if (save.insertedCount !== 1) {
+                    console.log("Not inserted");
+                } else {
+                    console.log(userInfo[0] + " registered.");
+                }
+            });
+}
 module.exports.findUser = findUser;
+module.exports.registerUser = registerUser;
--- register.js
+++ register.js
@@ -1,5 +1,5 @@
 var fs = require('fs');
-
+var db = require('./includes/db.js');
 function main(io, socket) {
     fs.readFile('./register.html', 'utf8', function (err, data) {
         if (err) {
@@ -8,7 +8,7 @@ function main(io, socket) {
         socket.emit('page', data);
     });
     socket.on('register', function (userInfo) {
-        
+        db.registerUser(userInfo);
     });
 }
 module.exports.main = main;
